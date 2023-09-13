513bb17099765c26395207221002f1260176f938
EMANUEL1035 <133671987+EMANUEL1035@users.noreply.github.com>
2023-09-13 10:30:27 -0500

    dr

---
+++ portafolio/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="robots" content="index,follow">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Operadores Aritmeticos</title>
+        <link rel="stylesheet" href="./estilos/style.css">
+        <link rel="stylesheet" href="./estilos/mobile.css" media="screen and (max-width: 1050px)"/>
+        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+    </head>
+    <body>
+    <header class="header">
+        <figure class="header_logo">
+            <img src="https://static.platzi.com/media/uploads/thumbnail_logo_platzi_ng_877f62a9f1.png" alt="Logo"></a>
+        </figure> 
+        <div class="header_atajos" id="_atajos">
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i> </i>  Nuestra plataforma </a></li>
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i> </i>  Montar un servivio </a></li>
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i> </i>  Buscar un servicio </a></li>
+            <li class="header_lista_atajos"><a href="portafolio.php" target="_blank"> <i class="fas fa-briefcase"></i>  Registrarse </a></li>
+            <li class="header_lista_atajos"><a href="cerrar.php" target="_blank"> <i class="fas fa-sign-out-alt"></i>  Iniciar sesión </a></li>
+        </div>
+        <div class="nav_toggle" id="_toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <script src="./codigo/script.js"></script>
+    </header>
+    </body>
+</html>
--- portafolio/index.php
+++ portafolio/index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="description" content="">
-        <meta name="robots" content="index,follow">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Operadores Aritmeticos</title>
-        <link rel="stylesheet" href="./estilos/style.css">
-    </head>
-    <body>
-        <p class="text">HOLA MUNDO ssss</p>
-    </body>
-</html>
+<?php include("header.php"); ?>
+
+hola soy el index
