aeed7e327473e15cd83831d13b795808228d3f48
EMANUEL1035 <133671987+EMANUEL1035@users.noreply.github.com>
2023-09-13 15:36:17 -0500

    hhhh

--- portafolio/footer.php
+++ portafolio/footer.php
@@ -8,15 +8,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Operadores Aritmeticos</title>
         <link rel="stylesheet" href="./estilos/style.css">
+        <link rel="stylesheet" href="./estilos/medida700.css" media="screen and (max-width: 700px)"/>
+        <link rel="stylesheet" href="./estilos/medida1100.css" media="screen and (max-width: 1100px)"/>
+        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     </head>
     <body>
         <footer class="footer">
-            <span class="footer_texto"> Redes Sociales <i class="fas fa-arrow-right"></i></span> 
-            <ul class="footer_redes_sociales">
-                <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
-                <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
-                <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-instagram-square"></i></a></li>
-            </ul>   
+            <div class="footer_1">
+                <img src="https://static.platzi.com/static/images/footer/logo.png" alt="platzi">
+                <br>
+                <span class="footer_texto"> Transformamos la economía de nuestros <br>países entregando grandes servios</span>
+            </div>
+            <div class="footer_2"> 
+                <span class="footer_texto"> Redes Sociales </span> 
+                <ul class="footer_redes_sociales">
+                    <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
+                    <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
+                    <li class="footer_lista_redes_sociales"><a href="" target="_blank"><i class="fab fa-instagram-square"></i></a></li>
+                </ul> 
+            </div>   
         </footer>  
     </body>
 </html>
--- portafolio/header.php
+++ portafolio/header.php
@@ -8,7 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Operadores Aritmeticos</title>
         <link rel="stylesheet" href="./estilos/style.css">
-        <link rel="stylesheet" href="./estilos/mobile.css" media="screen and (max-width: 1050px)"/>
+        <link rel="stylesheet" href="./estilos/medida700.css" media="screen and (max-width: 700px)"/>
+        <link rel="stylesheet" href="./estilos/medida1100.css" media="screen and (max-width: 1100px)"/>
         <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     </head>
     <body>
@@ -17,11 +18,11 @@
             <img src="https://static.platzi.com/media/uploads/thumbnail_logo_platzi_ng_877f62a9f1.png" alt="Logo"></a>
         </figure> 
         <div class="header_atajos" id="_atajos">
-            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i>  Nuestra plataforma </a></li>
-            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i>   Montar un servivio </a></li>
-            <li class="header_lista_atajos"><a href="index.php" target="_blank"> <i class="fas fa-home"></i>   Buscar un servicio </a></li>
-            <li class="header_lista_atajos"><a href="portafolio.php" target="_blank"> <i class="fas fa-briefcase"></i>  Registrarse </a></li>
-            <li class="header_lista_atajos"><a href="cerrar.php" target="_blank"> <i class="fas fa-sign-out-alt"></i>  Iniciar sesión </a></li>
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"><div class="header_lista_atajos_2"><i class="fas fa-tv"></i></div> Nuestra plataforma </a></li>
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"><div class="header_lista_atajos_2"><i class="fas fa-sort-amount-up"></i></div> Montar un servivio </a></li>
+            <li class="header_lista_atajos"><a href="index.php" target="_blank"><div class="header_lista_atajos_2"><i class="fas fa-search"></i></div> Buscar un servicio </a></li>
+            <li class="header_lista_atajos"><a href="portafolio.php" target="_blank"><div class="header_lista_atajos_2"><i class="fas fa-address-card"></i></div> Registrarse </a></li>
+            <li class="header_lista_atajos"><a href="cerrar.php" target="_blank"><div class="header_lista_atajos_2"><i class="fas fa-door-open"></i></div> Iniciar sesión </a></li>
         </div>
         <div class="nav_toggle" id="_toggle">
             <span></span>
