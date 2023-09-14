2425fe401585158f811ff66724462c6461f7c944
EMANUEL1035 <133671987+EMANUEL1035@users.noreply.github.com>
2023-09-14 09:56:26 -0500

    ddd

--- portafolio/index.php
+++ portafolio/index.php
@@ -7,11 +7,11 @@
         <meta name="robots" content="index,follow">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Operadores Aritmeticos</title>
-        <link rel="stylesheet" href="../portafolio/estilos/style.css">
-        <link rel="stylesheet" href="../portafolio/estilos/medida700.css" media="screen and (max-width: 700px)"/>
-        <link rel="stylesheet" href="../portafolio/estilos/medida1100.css" media="screen and (max-width: 950px)"/>
+        <link rel="stylesheet" href="../portafolio/estilos/estilos.css">
+        <link rel="stylesheet" href="./estilos/medida700.css" media="screen and (max-width: 700px)"/>
+        <link rel="stylesheet" href="./estilos/medida1100.css" media="screen and (max-width: 950px)"/>
         <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     </head>
-    <body>
+    <body class="fondo1">
     </body>
 </html>    
